284cd0da0f1d239abde7a668cc03e3f354b32ea4
jmcb7344 <95278683+jmcb7344@users.noreply.github.com>
2022-02-18 23:36:09 -0300

    Vista principal de la APP}

--- sap/urls.py
+++ sap/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from sap import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rrhh', include('app_RRHH.urls')),
+    path('', views.home, name='home')
 ]
