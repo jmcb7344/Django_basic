c7d29b695b0400cde1ad0677c4582f4c3e20e41c
jmcb7344 <95278683+jmcb7344@users.noreply.github.com>
2022-02-20 22:03:28 -0300

    Cracion de vistas

--- app_RRHH/urls.py
+++ app_RRHH/urls.py
@@ -4,5 +4,6 @@ from app_RRHH import views
 app_name='app_RRHH'
 
 urlpatterns = [
-    path('', views.RrhhView.as_view(), name='rrhhhome')
+    path('', views.RrhhView.as_view(), name='rrhhhome'),
+    path('<int:pk>/',views.DetalleView.as_view(), name='detalle'),
 ]
--- app_RRHH/views.py
+++ app_RRHH/views.py
@@ -5,11 +5,26 @@ from app_RRHH import models
 # Create your views here.
 class RrhhView(generic.View):
     cont = models.Empleado.objects.count()
+    activos = models.Empleado.objects.filter(activo=True).count
+    noactv = models.Empleado.objects.filter(activo=False).count
+    empleado = models.Empleado.objects.order_by('id')
     def get(self, request, *args, **kwargs):
         context = {
             'title':'RRHH',
             'contar':self.cont,
+            'activo':self.activos,
+            'noactv':self.noactv,
+            'empleado':self.empleado,
         }
         return render(request, 'rrhh.html', context)
 
 
+class DetalleView(generic.DetailView):
+    model = models.Empleado
+
+    """def get(self, request,id, *args, **kwargs):
+        empleado = self.model.objects.get(pk=id)
+        context ={
+            'persona':empleado
+        }
+        return render(request, 'detalle.html', context)"""
